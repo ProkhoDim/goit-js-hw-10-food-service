27ccd1c0e77a9487e256cfcc066d9cddadf8ac65
Dmitrij Prokhorenko <dmitrij.prokhorenko@gmail.com>
2020-02-02 12:28:45 +0200

    all done

--- src/jscript/app/script.js
+++ src/jscript/app/script.js
@@ -5,3 +5,32 @@ const jsMenu = document.querySelector('.js-menu');
 const markup = itemsArray.map(item => markupTemplate(item)).join('');
 
 jsMenu.insertAdjacentHTML('beforeend', markup);
+
+const Theme = {
+  LIGHT: 'light-theme',
+  DARK: 'dark-theme',
+};
+const bodyClassList = document.body.classList;
+const jsInputCheckbox = document.querySelector('.js-switch-input');
+
+bodyClassList.add(localStorage.getItem('CurrentTheme'));
+
+if (bodyClassList.value === Theme.DARK) jsInputCheckbox.checked = true;
+
+function handleClick(e) {
+  let isChecked = e.target.checked;
+
+  if (isChecked) {
+    isChecked = false;
+    localStorage.setItem('CurrentTheme', Theme.DARK);
+    bodyClassList.remove(Theme.LIGHT);
+    return bodyClassList.add(Theme.DARK);
+  }
+
+  isChecked = true;
+  bodyClassList.remove(Theme.DARK);
+  localStorage.setItem('CurrentTheme', Theme.LIGHT);
+  return bodyClassList.add(Theme.LIGHT);
+}
+
+jsInputCheckbox.addEventListener('change', handleClick);
